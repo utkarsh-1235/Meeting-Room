42fa40e4708300839c422075e79dce2e2f2a5ecf
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-06-03 22:44:14 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React,{ useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MeetingApp from './MeetingApp'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <MeetingApp/>
+    </>
+  )
+}
+
+export default App
